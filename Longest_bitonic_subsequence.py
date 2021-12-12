3a9005660279fc79aa92e47cd9d2cd15673cf8e9
spark1708 <53866998+spark1708@users.noreply.github.com>
2021-12-12 18:25:46 +0530

    Update Longest_bitonic_subsequence.py

--- Longest_bitonic_subsequence.py
+++ Longest_bitonic_subsequence.py
@@ -4,8 +4,10 @@ def long_bitonic_susequence(arr):
     for i in range(l-1):
         for j in range(i+1,l):
             if arr[i]<arr[j]:
+               # for the forward check of longest subsequence
                 forward[j] = max(forward[i]+1, forward[j])
             if arr[l-i-1]<arr[l-j-1]:
+               # for the backward check of longest subsequence
                 backward[l-j-1] = max(backward[l-i-1]+1, backward[l-j-1])
     res = 0
     for i in range(l):
