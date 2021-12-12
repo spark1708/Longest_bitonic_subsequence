018e802f5e6039c1231b52bad0125de72c9bf69a
spark1708 <53866998+spark1708@users.noreply.github.com>
2021-12-12 18:22:15 +0530

    Add files via upload
    Given an array of positive integers. Find the maximum length of Bitonic subsequence. 
    A subsequence of array is called Bitonic if it is first increasing, then decreasing.
    ex - nums = [1, 2, 5, 3, 2]
    Output: 5
    Explanation: The sequence {1, 2, 5} is
    increasing and the sequence {3, 2} is 
    decreasing so merging both we will get 
    length 5.

---
+++ Longest_bitonic_subsequence.py
@@ -0,0 +1,22 @@
+#Longest_bitonic_subsequence
+def long_bitonic_susequence(arr):
+     
+    for i in range(l-1):
+        for j in range(i+1,l):
+            if arr[i]<arr[j]:
+                forward[j] = max(forward[i]+1, forward[j])
+            if arr[l-i-1]<arr[l-j-1]:
+                backward[l-j-1] = max(backward[l-i-1]+1, backward[l-j-1])
+    res = 0
+    for i in range(l):
+        res = max(forward[i]+backward[i]-1,res)
+    return res
+                
+    
+    
+    
+arr = list(map(int,input().split()))
+l = len(arr)
+forward = [1]*l
+backward = [1]*l
+print(long_bitonic_susequence(arr))
